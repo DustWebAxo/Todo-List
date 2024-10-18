e557c1ddc52dd699cb2d2504ea708ae7c1cd48b2
Guilherme Pontes <gui12072004@gmail.com>
2024-10-18 15:10:07 -0300

    Update tasks

--- frontend/stores/todoStore.js
+++ frontend/stores/todoStore.js
@@ -6,16 +6,19 @@ export const useTodoStore = defineStore('todoStore', {
     todo: [],
     error: null,
   }),
+  
   actions: {
     async fetchTodo() {
       try {
         const { data, error } = await useFetch('http://localhost:3000/api');
         this.todo = data.value; 
         this.error = error.value;
+
       } catch (err) {
         this.error = err.message;
       }
     },
+
     async addTodo(todo) {
       try {
         const { data, error } = await useFetch('http://localhost:3000/api', {
@@ -24,10 +27,12 @@ export const useTodoStore = defineStore('todoStore', {
         });
         this.todo.push(data.value);
         this.error = error.value;
+        
       } catch (err) {
         this.error = err.message;
       }
     },
+
     async updateTodo(todoId, updatedTodo) {
       try {
         const { data, error } = await useFetch(`http://localhost:3000/api/${todoId}`, {
@@ -37,16 +42,19 @@ export const useTodoStore = defineStore('todoStore', {
         const index = this.todo.findIndex(todo => todo._id === todoId);
         this.todo[index] = data.value;
         this.error = error.value;
+
       } catch (err) {
         this.error = err.message;
       }
     },
+
     async deleteTodo(todoId) {
       try {
         await useFetch(`http://localhost:3000/api/${todoId}`, {
           method: 'DELETE',
         });
         this.todo = this.todo.filter(todo => todo._id !== todoId);
+
       } catch (err) {
         this.error = err.message;
       }
