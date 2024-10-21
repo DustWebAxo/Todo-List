ccdbfa52faf8ebc9c6fca8f41874beae5ff44e0b
g-pontes <gui12072004@gmail.com>
2024-10-21 20:32:56 -0300

    Update frontend

--- frontend/components/Titulo.vue
+++ frontend/components/Titulo.vue
@@ -1,7 +1,13 @@
 !<template>
-  <div>
+  <nav class="mx-auto py-8 border-b-2 border-gray-300">
+    <div class="flex justify-between max-w-[1800px] mx-auto">
+      <div class="text-xl font-bold">DustWeb.</div>
+      <div class="text-xl font-bold">Perfil</div>
+    </div>
+  </nav>
+  <!-- <div>
     <h1 class="text-4xl text-center font-bold py-7 bg-black text-white sm:text-5xl lg:text-6xl">
       TODO-LIST
     </h1>
-  </div>
+  </div> -->
 </template>
